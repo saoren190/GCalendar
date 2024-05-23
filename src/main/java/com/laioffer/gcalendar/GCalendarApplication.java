5d5c85f8e4134dfc537f481cc21e8e793d73f5f6
wangq <wangquanze922@gmail.com>
2024-05-23 10:22:22 -0500

    first commit

---
+++ src/main/java/com/laioffer/gcalendar/GCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.laioffer.gcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laioffer/gcalendar/GCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laioffer.gcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
